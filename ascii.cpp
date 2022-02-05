59c79953b3e565cf8cf85adbd7d906e0c3496b6a
maximmat <maximmatous@seznam.cz>
2022-02-05 19:02:01 +0100

    Upload

---
+++ ascii.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main() {
+	FILE* soubor = fopen("vystup.txt", "w");
+
+	if (soubor == NULL) {
+		printf("Chyba: Soubor nelze otevřít pro zápis.\n");
+		return 1;
+	}
+
+	int ascii = 0x40;
+
+	fprintf(soubor, "Znak-Hex\n");
+
+	for (int i = 0; i < 26; i++) {
+		ascii += 1;
+		fwrite(&ascii, sizeof(int), 1, soubor);
+		fprintf(soubor, "0x%x\n", ascii);
+	}
+
+	if (fclose(soubor) == EOF) {
+		printf("Chyba: Soubor se nepodařilo otevřít.\n");
+		return 1;
+	}
+
+	return(0);
+}
